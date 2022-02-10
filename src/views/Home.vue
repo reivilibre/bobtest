10140d979e6d6784f720c41564760570fed0d063
Olivier 'reivilibre <olivier@librepush.net>
2022-02-10 08:57:29 +0000

    Convert to <script setup>. Notice TypeScript checking seems to not bother now

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,14 +5,10 @@
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
+<script setup lang="ts">
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
-export default defineComponent({
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
-});
+const name = "Home";
+
+const bad: number = "badness";
 </script>
